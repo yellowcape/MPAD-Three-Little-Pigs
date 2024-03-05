5f8637d4877d8943d0bfef13949a034d11722609
Alex Overton <alexoverton3@cmail.carleton.ca>
2024-03-04 20:08:31 -0500

    these are the initial js and css files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+/**
+ * This function calculates the sum of two numbers.
+ * @param {number} num1 - The first number.
+ * @param {number} num2 - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
